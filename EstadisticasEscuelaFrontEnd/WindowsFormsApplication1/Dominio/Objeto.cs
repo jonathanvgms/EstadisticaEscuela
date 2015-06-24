7375e4ee09efa86fcaf35a8dc6159b37df1c845b
jonathanvgms <jonathanvgms@gmail.com>
2015-06-24 09:14:20 -0300

    asda

--- EstadisticasEscuelaFrontEnd/WindowsFormsApplication1/Dominio/Objeto.cs
+++ EstadisticasEscuelaFrontEnd/WindowsFormsApplication1/Dominio/Objeto.cs
@@ -7,7 +7,7 @@ namespace EstadisticasEscuelaFrontEnd.Dominio
 {
     class Objeto
     {
-        /*lalaalal*/
+        /*lalaalabdasmnbdasl*/
         private string tipo;
 
         public string Tipo
